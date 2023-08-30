4ac5070dedba9cf6d3f9495dc97d8fe4fe47b668
Mohamed Benkedadra <fennecinspace@gmail.com>
2023-08-30 10:46:44 +0200

    deployement 1

--- deepilia/main/urls.py
+++ deepilia/main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from main import views
 
 urlpatterns = [
+    path('', views.team_view, name='team'),
     path('team/', views.team_view, name='team'),
     path('team/<str:username>/', views.ProfileView.as_view(), name='profile'),
 
