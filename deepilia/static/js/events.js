bf166255208767a3e5d70db225a9dfad8555fdb3
Mohamed Benkedadra <fennecinspace@gmail.com>
2023-08-31 19:43:19 +0200

    removed language picker

--- deepilia/static/js/events.js
+++ deepilia/static/js/events.js
@@ -1,27 +1,27 @@
-document.addEventListener('click', (event) => {
-    var lng_picker = document.querySelector('#lng_picker > div > a');
-    if (lng_picker && event.target != lng_picker && lng_picker.hasAttribute('opened')){
-        lng_picker.removeAttribute('opened');
-        document.querySelector('#language_picker').style.display = 'none';
-    }
+// document.addEventListener('click', (event) => {
+//     var lng_picker = document.querySelector('#lng_picker > div > a');
+//     if (lng_picker && event.target != lng_picker && lng_picker.hasAttribute('opened')){
+//         lng_picker.removeAttribute('opened');
+//         document.querySelector('#language_picker').style.display = 'none';
+//     }
 
-    var edition_selector = document.querySelector('#edition_selector > div');
-    if (edition_selector && event.target != edition_selector && edition_selector.hasAttribute('opened')){
-        edition_selector.removeAttribute('opened');
-        document.querySelector('#edition_picker').style.display = 'none';
-    }
-});
+//     var edition_selector = document.querySelector('#edition_selector > div');
+//     if (edition_selector && event.target != edition_selector && edition_selector.hasAttribute('opened')){
+//         edition_selector.removeAttribute('opened');
+//         document.querySelector('#edition_picker').style.display = 'none';
+//     }
+// });
 
-document.querySelector('#lng_picker > div > a').addEventListener('click', function () {
-    if (this.hasAttribute('opened')) {
-        document.querySelector('#language_picker').style.display = 'none';
-        this.removeAttribute('opened');
-    }
-    else {
-        document.querySelector('#language_picker').style.display = 'grid';
-        this.setAttribute('opened', '');
-    }
-});
+// document.querySelector('#lng_picker > div > a').addEventListener('click', function () {
+//     if (this.hasAttribute('opened')) {
+//         document.querySelector('#language_picker').style.display = 'none';
+//         this.removeAttribute('opened');
+//     }
+//     else {
+//         document.querySelector('#language_picker').style.display = 'grid';
+//         this.setAttribute('opened', '');
+//     }
+// });
 
 document.querySelector('.hamburger').addEventListener('click', function () {
     if (this.classList.contains('is-active')) {
